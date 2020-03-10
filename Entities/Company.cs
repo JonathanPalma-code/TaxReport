1b9b53da65ddb2474195e99feface8b34358a3d9
Jonathan <jonathanpalma89@gmail.com>
2020-03-10 16:56:02 +0000

    chore: add company subclass members

--- Entities/Company.cs
+++ Entities/Company.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace TaxReport.Entities
 {
-    class Company
+    class Company : TaxPayer
     {
+        public int NumberOfEmployees { get; set; }
+
+        public Company(string name, double annualIncome, int numberOfEmployees)
+            : base(name, annualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            double total = (NumberOfEmployees <= 10) ?
+                (AnnualIncome * .16) :
+                (AnnualIncome * .14);
+            return total;
+        }
     }
 }
