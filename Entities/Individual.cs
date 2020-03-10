4db040647d2ae45c19e79cd296cd045368789a09
Jonathan <jonathanpalma89@gmail.com>
2020-03-10 16:41:10 +0000

    chore: add Individual subclass members

--- Entities/Individual.cs
+++ Entities/Individual.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace TaxReport.Entities
 {
-    class Individual
+    class Individual : TaxPayer
     {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double annualIncome, double healthExpenditures)
+            : base(name, annualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            double total = (AnnualIncome < 20000) ?
+                (AnnualIncome * .15) - (HealthExpenditures * .50) :
+                (AnnualIncome * .25) - (HealthExpenditures * .50);
+            return total;
+        }
     }
 }
